85bf9f3e01ed32b80e93d00bef939541cde522be
metalboroda <89025071+metalboroda@users.noreply.github.com>
2021-08-20 13:00:55 +0300

    20.08.2021 Work

--- Assets/Scripts/UI/AudioVolumeController.cs
+++ Assets/Scripts/UI/AudioVolumeController.cs
@@ -6,36 +6,19 @@ namespace UI
 {
     public class AudioVolumeController : MonoBehaviour
     {
-        public static AudioVolumeController instance = null;
+        public static AudioVolumeController Instance = null;
 
         public Button soundtrackButton;
         public bool isSoundtrackPlay = true;
         public AudioMixerGroup soundtrack;
-
-
-        /*public void SoundtrackEnableSwitch()
-        {
-            if (isSoundtrackPlay)
-            {
-                soundtrackButton.image.color = new Color(1f, 1f, 1f, 0.39f);
-                soundtrack.audioMixer.SetFloat("Soundtrack", -80.0f);
-                isSoundtrackPlay = false;
-            }
-            else if (!isSoundtrackPlay)
-            {
-                soundtrackButton.image.color = Color.white;
-                soundtrack.audioMixer.SetFloat("Soundtrack", -0.0f);
-                isSoundtrackPlay = true;
-                Debug.Log("FALSE");
-            }
-        }*/
 
         public void ToggleMusicMethod()
         {
             if (PlayerPrefs.GetInt("Muted", 0) == 0)
             {
                 PlayerPrefs.SetInt("Muted", 1);
-                soundtrackButton.image.color = new Color(1f, 1f, 1f, 0.39f);
+                // soundtrackButton.image.color = new Color(1f, 1f, 1f, 0.39f);
+                soundtrackButton.image.color = Color.gray;
                 soundtrack.audioMixer.SetFloat("Soundtrack", -80.0f);
             }
             else
